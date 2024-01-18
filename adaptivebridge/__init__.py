42ec54fdfa2e31137003dc4a8b862c1128072171
Netanel Eliav <netanel.eliav@gmail.com>
2024-01-18 20:10:21 +0000

    minor PEP changes

--- adaptivebridge/__init__.py
+++ adaptivebridge/__init__.py
@@ -1,11 +1,12 @@
 #!/bin/env python
 # adaptivebridge/__init__.py
+
 """
-    Package Name: AdaptiveBridge
-    Author: Netanel Eliav
-    Author Email: netanel.eliav@gmail.com
-    License: MIT License
-    Version: Please refer to the repository for the latest version and updates.
+Package Name: AdaptiveBridge
+Author: Netanel Eliav
+Author Email: netanel.eliav@gmail.com
+License: MIT License
+Version: Please refer to the repository for the latest version and updates.
 """
 
 # module level doc-string
